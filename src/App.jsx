2685ef927e9b082c80433b6afe2c97ed31a457ec
loal1018 <loal1018@h-ka.de>
2024-06-02 17:35:34 +0200

    BookSearchForm erstellt

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
 import './App.css'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
+import BookSearch from './components/BookSearch';
 
 function App() {
-  const fetchData = async () => {
-    try {
-      const response = await fetch('/api/rest'); // Angenommen, dein Endpunkt ist /api/data
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const data = await response.json();
-      console.log(data); // Daten in der Konsole anzeigen
-    } catch (error) {
-      console.error('Es gab ein Problem mit der Fetch-Operation:', error);
-    }
-  };
+
 
   return (
     <>
       <Navbar />
-        <button onClick={fetchData}>
-          Daten laden
-        </button>
+        <BookSearch />
     </>
   )
 }
