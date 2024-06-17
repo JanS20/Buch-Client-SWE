fe5fd28858a2fa88f69b93fd372b04c95def9bdc
Jan Schneller <janschneller20@gmail.com>
2024-06-17 18:21:23 +0200

    AuthProvider clean up

--- src/components/provider/AuthProvider.jsx
+++ src/components/provider/AuthProvider.jsx
@@ -2,53 +2,53 @@ import { createContext, useState } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
-// eslint-disable-next-line import/exports-last
 export const AuthProvider = ({ children }) => {
-  const [cToken, setCToken] = useState('');
+  const [Token, setToken] = useState('');
   const [writeAccess, setWriteAccess] = useState(false);
 
   const login = async (username, password) => {
     try {
-      const response = await loginUser({ username, password });
-      const { token, roles } = response.data;
-      setCToken(token);
+      // destructuring
+      const { data: { token, roles } } = await loginUser({ username, password });
+      setToken(token);
 
-      const isAdminOrFachabteilung = roles.some(
-        (r) => r === 'admin' || r === 'fachabteilung'
-      );
+      const isAdmin = roles.includes('admin');
       const isKunde = roles.includes('kunde');
-      setWriteAccess(isAdminOrFachabteilung);
-      return isAdminOrFachabteilung || isKunde;
+      setWriteAccess(isAdmin);
+
+      return isAdmin || isKunde;
     } catch (error) {
       return false;
     }
   };
 
   const loginUser = async ({ username, password }) => {
-    const url = '/api/auth/login';
-    const requestData = `username=${encodeURIComponent(
-      username
-    )}&password=${encodeURIComponent(password)}`;
+    const url = '/auth/login';
+    const requestData = {
+      username: encodeURIComponent(username),
+      password: encodeURIComponent(password)
+    };
 
-    const response = await axios.post(url, requestData);
-    if (response.status === 200) {
+    try {
+      const response = await axios.post(url, requestData);
       return response;
-    } else {
+    } catch (error) {
+      //axios wirft standardmäßig Fehler wirft, wenn der Statuscode nicht im Bereich 200 liegt
       throw new Error('Login failed');
     }
   };
 
   const logout = () => {
-    setCToken('');
+    setToken('');
     setWriteAccess(false);
   };
 
   const isLoggedIn = () => {
-    return (cToken !== '')
+    return (Token !== '')
   }
 
   return (
-    <AuthContext.Provider value={{ cToken, writeAccess, login, logout, isLoggedIn }}>
+    <AuthContext.Provider value={{ Token, writeAccess, login, logout, isLoggedIn }}>
       {children}
     </AuthContext.Provider>
   );
