fa1872b14a0a60e84423d29464ef28cab3c23d84
Jan Schneller <janschneller20@gmail.com>
2024-06-25 22:47:50 +0200

    zugriff edit/add

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/navbar'; // Beachte den korrekten Pfad
 import BookEdit from './components/main/BookEdit'; // Beachte den korrekten Pfad
-import BookSearch from './components/main/BookSearch';
-import AddNewBook from './components/main/AddNewBook'; // Beachte den korrekten Pfad
+import AddNewBook from './components/main/AddNewBook'; 
+import BookSearch from './components/main/BookSearch';// Beachte den korrekten Pfad
 import BookDetails from './components/main/BookDetails'; // Stelle sicher, dass der Pfad korrekt ist
 import HomePage from './components/main/HomePage'; // Beachte den korrekten Pfad
-import LoginForm from './components/login';
+import { useAuth } from './components/provider/useAuth';
+
 function App() {
+  const { isLoggedIn } = useAuth();
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/search" element={<BookSearch />} />
-        <Route path="/edit/:id" element={<BookEdit />} />
-        <Route path="/add" element={<AddNewBook />} />
+        <Route path="/edit/:id" element={isLoggedIn() ? <BookEdit /> : <HomePage />} />
+        <Route path="/add" element={isLoggedIn() ? <AddNewBook /> : <HomePage />} />
         <Route path="/details/:id" element={<BookDetails />} />
-        <Route path="/login" element={<LoginForm />} />
       </Routes>
     </>
   );
